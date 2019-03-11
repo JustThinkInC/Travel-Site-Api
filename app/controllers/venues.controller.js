5d7e9566f4ec2a32c42963fec07e3a2d63fae78a
George K <gkh36@uclive.ac.nz>
2019-03-12 12:44:29 +1300

    Added venue function calls in controller

--- app/controllers/venues.controller.js
+++ app/controllers/venues.controller.js
@@ -1,7 +1,29 @@
 const Venues = require('../models/venues.model');
 
+// GET all data
 exports.view = function(req, res) {
     Venues.getAll(function(result) {
+        res.json(result);
+    })
+};
+
+// POST add a venue
+exports.add = function(req, res) {
+    Venues.insert(function(result) {
+        res.json(result);
+    })
+};
+
+// GET a specific venue
+exports.viewVenue = function(req, res) {
+    Venues.getVenue(function(result) {
+        res.json(result);
+    })
+};
+
+// PATCH (update) a specific venue
+exports.updateVenue = function(req, res) {
+    Venues.patchVenue(function(result) {
         res.json(result);
     })
 };
