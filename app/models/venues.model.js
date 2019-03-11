42769a93a59e274c89277dbb9ab2c37fdc140810
George K <gkh36@uclive.ac.nz>
2019-03-12 12:44:01 +1300

    Added template functions for venues

--- app/models/venues.model.js
+++ app/models/venues.model.js
@@ -1,8 +1,22 @@
 const db = require('../../config/db');
 
+const unimplemented = {"Error" : "Unimplemented"};  // Unimplemented error message in JSON
+
 exports.getAll = function(req, res) {
     db.getPool().query("SELECT * FROM Venue WHERE city = ?, ", function(err, rows) {
         if (err) return done(err);
         res.json(rows);
     });
 };
+
+exports.insert = function(req, res) {
+   res.json(unimplemented);
+};
+
+exports.getVenue = function(req, res) {
+    res.json(unimplemented);
+};
+
+exports.patchVenue = function(req, res) {
+    res.json(unimplemented);
+}
