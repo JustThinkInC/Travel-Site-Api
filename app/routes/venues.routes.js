e6a67dfd5ac61bfb167f4e3aa5631ae5d7c3f12b
George K <gkh36@uclive.ac.nz>
2019-03-12 12:42:37 +1300

    Added routes for venues endpoint
    Routes:
    - /venues
    - /venues/{id}
    - /categories

--- app/routes/venues.routes.js
+++ app/routes/venues.routes.js
@@ -1,7 +1,17 @@
 const venues = require('../controllers/venues.controller');
 
 module.exports = function (app) {
-    app.route(app.rootUrl+'/venues')
-        .get(venues.view);
+    // General venues
+    app.route(app.rootUrl + '/venues')
+        .get(venues.view)
+        .post(venues.add);
 
+    // Specific venue
+    app.route(app.rootUrl + '/venues/:id')
+        .get(venues.viewVenue)
+        .patch(venues.updateVenue);
+
+    // All info about venue categories
+    app.route(app.rootUrl + '/categories')
+        .get(venues.categories);
 };
