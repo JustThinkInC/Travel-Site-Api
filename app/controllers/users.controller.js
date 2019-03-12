230c35eecdc301c081777b2d9d715502265a3905
George K <gkh36@uclive.ac.nz>
2019-03-12 23:17:54 +1300

    Added status codes, using async for registering
    Status codes will only work with success cases at the moment

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -1,8 +1,15 @@
 const Users = require('../models/users.model');
 
 // POST: register a user
-exports.register = function(req, res) {
-    Users.insert(req.body, function(result) {
-        res.json(result);
-    })
+exports.register = async function(req, res) {
+    try {
+        let result = await Users.insert(req.body, function(result) {
+            res.json(result);
+        });
+        res.statusMessage = 'Created';
+        res.status(201);
+    } catch (err) {
+        res.statusMessage = 'Bad Request';
+        res.status(401);
+    }
 };
