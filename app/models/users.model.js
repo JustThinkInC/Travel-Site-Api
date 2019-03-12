c56330b26c51485214efa0d03d88d00b1c778898
George K <gkh36@uclive.ac.nz>
2019-03-13 10:29:49 +1300

    User insert function now async, removed callback
    Part of fix for POST /users

--- app/models/users.model.js
+++ app/models/users.model.js
@@ -2,15 +2,11 @@ const db = require('../../config/db');
 
 const unimplemented = {"Error" : "Unimplemented"};
 
-exports.insert = function(body, done) {
+exports.insert = async function(body) {
     let info = [[body.username, body.email, body.givenName, body.familyName, body.password]];
     for(let i=0; i < info.length; i++) {
         if (typeof info[i] === undefined) return done({400:"Bad Request"});
     }
 
-    db.getPool().query('INSERT INTO User(username, email, given_name, family_name, password) VALUES ?', [info], function(err, result) {
-            if (err) return done({400:"Bad Request"});
-
-            return done ({201: "Created"});
-        });
+    return await db.getPool().query('INSERT INTO User(username, email, given_name, family_name, password) VALUES ?', [info]);
 };
