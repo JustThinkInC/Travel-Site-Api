2146336ee476616e7d880cd0263667ddc0bee508
George K <gkh36@uclive.ac.nz>
2019-03-13 11:17:07 +1300

    Corrected typeof check on array elements
    Helps to fix errors with invalid data on POST /users

--- app/models/users.model.js
+++ app/models/users.model.js
@@ -3,10 +3,11 @@ const db = require('../../config/db');
 const unimplemented = {"Error" : "Unimplemented"};
 
 exports.insert = async function(body) {
-    let info = [[body.username, body.email, body.givenName, body.familyName, body.password]];
+    let info = [body.username, body.email, body.givenName, body.familyName, body.password];
     for(let i=0; i < info.length; i++) {
-        if (typeof info[i] === undefined) return done({400:"Bad Request"});
-    }
+        if (typeof info[i] === "undefined") throw ("Missing field");
+    };
 
-    return await db.getPool().query('INSERT INTO User(username, email, given_name, family_name, password) VALUES ?', [info]);
+    return await db.getPool().query('INSERT INTO User(username, email, given_name, family_name, password)' +
+        ' VALUES ?', [[info]]);
 };
