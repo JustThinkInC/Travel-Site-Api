df84381f2fa86f6506440fb020cceeed94003694
George K <gkh36@uclive.ac.nz>
2019-03-12 22:39:48 +1300

    Require routes files
    Commented out unimplemented ones

--- config/express.js
+++ config/express.js
@@ -19,6 +19,11 @@ module.exports = function () {
 
     // ROUTES
     require('../app/routes/backdoor.routes')(app);
+    require('../app/routes/venues.routes')(app);
+    // require('../app/routes/venues.photos.routes')(app);
+     require('../app/routes/users.routes')(app);
+    // require('../app/routes/users.photos.routes')(app);
+    // require('../app/routes/reviews.routes')(app);
 
     // DEBUG (you can remove this)
     app.get('/', function (req, res) {
