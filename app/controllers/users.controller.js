be11f825098a8bffd46b056e69ce38f2ed7f499a
George K <gkh36@uclive.ac.nz>
2019-03-13 19:47:53 +1300

    Corrected JSON response
    JSON response now {"userId": <ID>}
    Should resolve failure on POST /users

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -3,11 +3,11 @@ const Users = require('../models/users.model');
 // POST: register a user
 exports.register = async function(req, res) {
     try {
-        let result = await Users.insert(req.body).insertId;
+        let result = await Users.insert(req.body);
         res.setHeader("Content-Type", "application/json");
         res.statusMessage = 'Created';
         res.status(201);
-        res.json("Created");
+        res.json({"userId":JSON.stringify(result.insertId)});
     } catch (err) {
         res.setHeader("Content-Type", "application/json");
         res.statusMessage = 'Bad Request';
