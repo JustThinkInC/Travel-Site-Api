c8c4d630c33892375f2b33bdbd51ee34f4e28b42
George K <gkh36@uclive.ac.nz>
2019-03-13 16:04:05 +1300

    Added json message in response
    Implements POST /user

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -5,9 +5,11 @@ exports.register = async function(req, res) {
     try {
         let result = await Users.insert(req.body);
         res.statusMessage = 'Created';
+        res.json("Created");
         res.sendStatus(201);
     } catch (err) {
         res.statusMessage = 'Bad Request';
+        res.json("Bad Request");
         res.sendStatus(400);
     }
 };
