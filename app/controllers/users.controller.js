33ec05ff1d84efab4eb86501bfb8f5e658d903f2
George K <gkh36@uclive.ac.nz>
2019-03-13 19:41:24 +1300

    Set result to be insertId
    May resolve unhelpful error message:
     - expected undefined to equal 13

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -3,7 +3,7 @@ const Users = require('../models/users.model');
 // POST: register a user
 exports.register = async function(req, res) {
     try {
-        let result = await Users.insert(req.body);
+        let result = await Users.insert(req.body).insertId;
         res.setHeader("Content-Type", "application/json");
         res.statusMessage = 'Created';
         res.status(201);
