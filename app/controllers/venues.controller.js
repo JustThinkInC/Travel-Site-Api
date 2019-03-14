422a925a90f72a7020621b26b00e4bbfb98b0a35
George K <gkh36@uclive.ac.nz>
2019-03-14 18:39:09 +1300

    Controller method for adding venues
    Implements POST /venues

--- app/controllers/venues.controller.js
+++ app/controllers/venues.controller.js
@@ -2,16 +2,36 @@ const Venues = require('../models/venues.model');
 
 // GET all data
 exports.view = function(req, res) {
-    Venues.getAll(function(result) {
+    let values = [req.params.startIndex, req.params.count, req.params.city,
+                  req.params.q, req.params.catergoryId, req.params.minStarRating,
+                  req.params.maxCostRating, req.params.adminId, req.params.sortBy,
+                  req.params.reverseSort, req.params.myLatitude, req.params.myLongitude]
+    Venues.getAll(values,function(result) {
         res.json(result);
-    })
+    });
 };
 
 // POST add a venue
-exports.add = function(req, res) {
-    Venues.insert(function(result) {
-        res.json(result);
-    })
+exports.add = async function(req, res) {
+    try {
+        let result = await Venues.insert(req.headers, req.body);
+        res.setHeader("Content-Type", "application/json");
+        res.statusMessage = 'Created';
+        res.status(201);
+        res.json({"venueId" : result.insertId});
+    } catch (err) {
+        console.log(err);
+        res.setHeader("Content-Type", "application/json");
+        if (err.name === "Unauthorized") {
+            res.statusMessage =  "Unauthorized";
+            res.status = 401;
+            res.json("Unauthorized");
+        } else {
+            res.statusMessage = 'Bad Request';
+            res.status(400);
+            res.json("Bad Request");
+        }
+    }
 };
 
 // GET a specific venue
