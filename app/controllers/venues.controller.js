828d12708219f27a07f434d2622a5cf02e376b31
George K <gkh36@uclive.ac.nz>
2019-03-15 09:27:09 +1300

    Controller function for PATCH /venues/:id

--- app/controllers/venues.controller.js
+++ app/controllers/venues.controller.js
@@ -49,11 +49,33 @@ exports.viewVenue = async function(req, res) {
     }
 };
 
+
 // PATCH (update) a specific venue
-exports.updateVenue = function(req, res) {
-    Venues.patchVenue(function(result) {
-        res.json(result);
-    })
+exports.updateVenue = async function(req, res) {
+    try {
+        let result = await Venues.patchVenue(req);
+        res.setHeader("Content-Type", "application/json");
+        res.statusMessage = 'OK';
+        res.status(200);
+        res.json();
+    } catch (err) {
+        res.setHeader("Content-Type", "application/json");
+        res.statusMessage = err.name;
+        switch (err.name) {
+            case "Bad Request":
+                res.status(400);
+                break;
+            case "Unauthorized":
+                res.status(401);
+                break;
+            case "Forbidden":
+                res.status(403);
+                break;
+            case "Not Found":
+                res.status(404);
+        }
+        res.json();
+    }
 };
 
 
