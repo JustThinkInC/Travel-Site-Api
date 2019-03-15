2d8718c34eb4ee2859599165850af0c2cb69e227
George K <gkh36@uclive.ac.nz>
2019-03-15 17:26:26 +1300

    Controller method for PATCH /users/:id

--- app/controllers/users.controller.js
+++ app/controllers/users.controller.js
@@ -68,3 +68,32 @@ exports.getUser = async function(req, res) {
         res.json("Not Found");
     }
 };
+
+
+// PATCH (update) a specific venue
+exports.patchUser = async function(req, res) {
+    try {
+        let result = await Users.patchUser(req);
+        res.setHeader("Content-Type", "application/json");
+        res.statusMessage = 'OK';
+        res.status(200);
+        res.json();
+    } catch (err) {
+        res.setHeader("Content-Type", "application/json");
+        res.statusMessage = err.name;
+        switch (err.name) {
+            case "Bad Request":
+                res.status(400);
+                break;
+            case "Unauthorized":
+                res.status(401);
+                break;
+            case "Forbidden":
+                res.status(403);
+                break;
+            case "Not Found":
+                res.status(404);
+        }
+        res.json();
+    }
+};
