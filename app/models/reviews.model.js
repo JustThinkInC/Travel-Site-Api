d41377a68b3a6441f28c8ca80412e4b0bf9df99d
George K <gkh36@uclive.ac.nz>
2019-03-15 22:35:33 +1300

    Implementation of POST /venues/:id/reviews

---
+++ app/models/reviews.model.js
@@ -0,0 +1,47 @@
+const db = require('../../config/db');
+const AUTHERROR = {name:"Unauthorized", message:"Unauthorized"};
+const NOTFOUNDERROR = {name:"Not Found", message:"Not Found"};
+const FORBIDDENERROR = {name:"Forbidden", message:"Forbidden"};
+const BADREQUESTERROR = {name:"Bad Request", message:"Bad Request"};
+
+
+// POST: add a review for a venue
+exports.addReview = async function(req) {
+    const body = req.body;
+    let id = req.params.id;
+    let auth = req.headers["x-authorization"];
+    let user;
+
+    // Check authorisation
+    if (typeof auth === "undefined" || auth === "" || auth === null) {
+        throw AUTHERROR;
+    } else {
+        // Check user exists
+        user = await db.getPool().query("SELECT * FROM User WHERE auth_token = ?", [auth]);
+        if (typeof user[0] === "undefined") throw NOTFOUNDERROR;
+        // Check user is not admin of venue
+        if (user[0]["user_id"] === id) throw FORBIDDENERROR;
+        // Ensure user has not already reviewed said venue
+        let reviewedVenues = await db.getPool().query("SELECT reviewed_venue_id FROM Review WHERE review_author_id = ?",
+            user[0]["user_id"]);
+        reviewedVenues = JSON.parse(JSON.stringify(reviewedVenues));
+        for(let i=0; i < Object.keys(reviewedVenues).length; i++) {
+            if (reviewedVenues[i] === user[0]["user_id"]) throw FORBIDDENERROR;
+        }
+    }
+
+    // Review details
+    let review = [id, user[0]["user_id"], body["reviewBody"], body["starRating"], body["costRating"], new Date()];
+
+    // Ensure all details exist
+    for (let i=0; i < review.length; i++) {
+        if (typeof review[i] === "undefined" || review[i] === "" || review[i] === null) throw BADREQUESTERROR;
+    }
+
+    // Check ratings are valid: non-decimal, and between 0 to 5 inclusive
+    if (!(0 <= review["star_rating"] <= 5 && 0 <= review["cost_rating"] <= 5)) throw BADREQUESTERROR;
+    if (review[3] % 1 !== 0 || review[4] % 1 !== 0) throw BADREQUESTERROR;
+
+    return await db.getPool().query("INSERT INTO Review(reviewed_venue_id, review_author_id, review_body, star_rating," +
+        "cost_rating, time_posted) VALUES ?", [[review]]);
+};
