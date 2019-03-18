78efd349cc7615be85456a6f59d7b5b41dafa664
George K <gkh36@uclive.ac.nz>
2019-03-19 12:17:40 +1300

    Implement GET /users/:id/photos

--- app/controllers/users.photos.controller.js
+++ app/controllers/users.photos.controller.js
@@ -9,7 +9,35 @@ exports.add = async function(req, res) {
         res.status(result["status"]);
         res.json();
     } catch (err) {
-        console.log(err)
+        res.setHeader("Content-Type", "application/json");
+        res.statusMessage = err.name;
+        switch (err.name) {
+            case "Bad Request":
+                res.status(400);
+                break;
+            case "Unauthorized":
+                res.status(401);
+                break;
+            case "Forbidden":
+                res.status(403);
+                break;
+            case "Not Found":
+                res.status(404);
+        }
+        res.json();
+    }
+};
+
+
+// GET a photo for a user
+exports.get = async function(req, res) {
+    try {
+        let result = await Photos.insert(req);
+        res.setHeader("Content-Type", "application/json");
+        res.statusMessage = result["message"];
+        res.status(result["status"]);
+        res.json();
+    } catch (err) {
         res.setHeader("Content-Type", "application/json");
         res.statusMessage = err.name;
         switch (err.name) {
