55f5b5b08852d085d0c26b3817341e4bf4a6053f
George K <gkh36@uclive.ac.nz>
2019-03-19 12:21:27 +1300

    Update response for GET /users/:id/photos

--- app/controllers/users.photos.controller.js
+++ app/controllers/users.photos.controller.js
@@ -32,27 +32,15 @@ exports.add = async function(req, res) {
 // GET a photo for a user
 exports.get = async function(req, res) {
     try {
-        let result = await Photos.insert(req);
-        res.setHeader("Content-Type", "application/json");
+        let result = await Photos.view(req.params.id);
+        res.setHeader("Content-Type", result["content"]);
         res.statusMessage = result["message"];
         res.status(result["status"]);
-        res.json();
+        res.json(result["image"]);
     } catch (err) {
         res.setHeader("Content-Type", "application/json");
-        res.statusMessage = err.name;
-        switch (err.name) {
-            case "Bad Request":
-                res.status(400);
-                break;
-            case "Unauthorized":
-                res.status(401);
-                break;
-            case "Forbidden":
-                res.status(403);
-                break;
-            case "Not Found":
-                res.status(404);
-        }
+        res.statusMessage = "Not Found";
+        res.status(404);
         res.json();
     }
 };
