f6ad638b62e9d7d4425117bc9eee7a48e94160d7
George K <gkh36@uclive.ac.nz>
2019-03-20 09:07:12 +1300

    Controller method to delete user photo

--- app/controllers/users.photos.controller.js
+++ app/controllers/users.photos.controller.js
@@ -45,3 +45,32 @@ exports.get = async function(req, res) {
         res.json();
     }
 };
+
+
+//DELETE a user photo
+exports.remove = async function(req, res) {
+    try {
+        let result = await Photos.delete(req);
+        res.setHeader("Content-Type", "application/json");
+        res.statusMessage = "OK";
+        res.status(200);
+        res.json();
+    } catch (err) {
+        res.setHeader("Content-Type", "application/json");
+        res.statusMessage = err.name;
+        switch (err.name) {
+            case "Bad Request":
+                res.status(400);
+                break;
+            case "Unauthorized":
+                res.status(401);
+                break;
+            case "Forbidden":
+                res.status(403);
+                break;
+            case "Not Found":
+                res.status(404);
+        }
+        res.json();
+    }
+};
