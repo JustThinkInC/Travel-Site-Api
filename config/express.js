1505a687120f78dc66f86cb871aa5430d1d05e78
George K <gkh36@uclive.ac.nz>
2019-03-19 20:13:40 +1300

    Create folders if not existing

--- config/express.js
+++ config/express.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const allowCrossOriginRequests = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -23,8 +24,16 @@ module.exports = function () {
     require('../app/routes/venues.routes')(app);
     // require('../app/routes/venues.photos.routes')(app);
     require('../app/routes/users.routes')(app);
-     require('../app/routes/users.photos.routes')(app);
+    require('../app/routes/users.photos.routes')(app);
     require('../app/routes/reviews.routes')(app);
+
+    if (!fs.existsSync("app/user.photos/")) {
+        fs.mkdirSync("app/user.photos/");
+    }
+
+    if (!fs.existsSync("app/venue.photos/")) {
+        fs.mkdirSync("app/venue.photos/");
+    }
 
     // DEBUG (you can remove this)
     app.get('/', function (req, res) {
