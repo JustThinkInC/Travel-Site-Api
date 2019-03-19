f8c188d9c346cbc2cb32fb38c77939cb8084eb9d
George K <gkh36@uclive.ac.nz>
2019-03-20 09:56:06 +1300

    Controller method for GET /users/:id/reviews

--- app/controllers/reviews.controller.js
+++ app/controllers/reviews.controller.js
@@ -44,3 +44,26 @@ exports.view = async function(req, res) {
         res.json();
     }
 };
+
+
+// GET reviews by specific user
+exports.getUserReviews = async function(req, res) {
+    try {
+        let result = await Reviews.getUserReviews(req);
+        res.setHeader("Content-Type", "application/json");
+        res.statusMessage = 'OK';
+        res.status(200);
+        res.json(result);
+    } catch (err) {
+        res.setHeader("Content-Type", "application/json");
+        res.statusMessage = err.name;
+        switch (err.name) {
+            case "Unauthorized":
+                res.status(401);
+                break;
+            case "Not Found":
+                res.status(404);
+        }
+        res.json();
+    }
+};
