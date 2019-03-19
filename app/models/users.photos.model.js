a2971940fa4f9d592ad14ef00e02d8d43f07737b
George K <gkh36@uclive.ac.nz>
2019-03-20 10:16:05 +1300

    Fix incorrect parameter & type check
    DELETE /users/:id/photo

--- app/models/users.photos.model.js
+++ app/models/users.photos.model.js
@@ -120,13 +120,13 @@ exports.delete =  async function(req) {
     // Check user exists if no photo found
     if (!photo[0]) {
         let userExist = await db.getPool().query("SELECT user_id FROM User WHERE user_id = ?", [id]);
-        if (typeof userExist[0]["user_id"] === "undefined") throw NOTFOUNDERROR;
+        if (typeof userExist[0] === "undefined") throw NOTFOUNDERROR;
     } else if (typeof dbAuth[0] !== "undefined" && dbAuth[0]["user_id"] !== id) {   // Check authentication matches user
         throw FORBIDDENERROR;
     }
 
     // Delete the photo
-    removePhoto(photo[0], photo[1]);
+    removePhoto(id, photo[1]);
 
     return await db.getPool().query("UPDATE User SET profile_photo_filename = '' WHERE user_id = ?", [id]);
 };
