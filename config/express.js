0dd0565d1f51190b0b53a4a8c3bc56cff6b1d670
George K <gkh36@uclive.ac.nz>
2019-03-20 20:25:13 +1300

    Multer middleware

--- config/express.js
+++ config/express.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
+const multer = require('multer');
+const storage = multer.memoryStorage();
+const upload = multer({storage:storage});
 
 const allowCrossOriginRequests = function (req, res, next) {
     res.header('Access-Control-Allow-Origin', '*');
@@ -18,19 +21,20 @@ module.exports = function () {
     app.use(bodyParser.json());
     app.use(bodyParser.raw({ type: 'text/plain' }));  // for the /executeSql endpoint
     app.use(bodyParser.raw({inflate:"true", limit:"5mb", type:["image/jpeg", "image/png"]}));
+    app.use(upload.single('photo'));    // Use multer package for multipart/form-data
 
     // ROUTES
     require('../app/routes/backdoor.routes')(app);
     require('../app/routes/venues.routes')(app);
-    // require('../app/routes/venues.photos.routes')(app);
+    require('../app/routes/venues.photos.routes')(app);
     require('../app/routes/users.routes')(app);
     require('../app/routes/users.photos.routes')(app);
     require('../app/routes/reviews.routes')(app);
 
+    // Create the photo directories if not exist
     if (!fs.existsSync("app/user.photos/")) {
         fs.mkdirSync("app/user.photos/");
     }
-
     if (!fs.existsSync("app/venue.photos/")) {
         fs.mkdirSync("app/venue.photos/");
     }
