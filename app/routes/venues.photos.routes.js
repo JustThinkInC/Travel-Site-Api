ecd10181496fe4b7452ff78a800f580ac2d2560f
George K <gkh36@uclive.ac.nz>
2019-03-21 15:34:35 +1300

    Route for delete venue photo

--- app/routes/venues.photos.routes.js
+++ app/routes/venues.photos.routes.js
@@ -3,8 +3,8 @@ const photos = require('../controllers/venues.photos.controller');
 module.exports = function (app) {
     // Venue reviews
     app.route(app.rootUrl + '/venues/:id/photos')
-        .post(photos.add);
-  //      .delete(photos.remove);
+        .post(photos.add)
+        .delete(photos.remove);
 
     app.route(app.rootUrl + '/venues/:id/photos/:photoFilename')
         .get(photos.get);
