aa2207436920813eb276668c22e63f0519b0186f
George K <gkh36@uclive.ac.nz>
2019-03-22 22:52:22 +1300

    Attempt to fix check on count

--- app/models/venues.model.js
+++ app/models/venues.model.js
@@ -207,7 +207,7 @@ exports.getAll = async function(values) {
     dbRes =  await db.getPool().query("SELECT Venue.venue_id, venue_name, category_id, city, short_description, latitude, longitude" +
             " FROM Venue, Review, ModeCostRating " + query + " " + qSearch);
     //if (typeof count === "undefined") count = dbRes.length;
-    for (let i=0; typeof dbRes[i] !== "undefined" && i !== count; i++) {
+    for (let i=0; typeof dbRes[i] !== "undefined" && i <= count; i++) {
         let starRatings = await db.getPool().query("SELECT AVG(star_rating) AS average FROM Review WHERE reviewed_venue_id = ?",
             [dbRes[i]["venue_id"]]);
         result.push({
@@ -226,9 +226,9 @@ exports.getAll = async function(values) {
 
     console.log("COUNT " + count)
     console.log(result);
-    if (startIndex >= result.length) {
-        return result;
-    }
+    // if (startIndex >= result.length) {
+    //     return result;
+    // }
     return result.slice(startIndex);
 };
 
