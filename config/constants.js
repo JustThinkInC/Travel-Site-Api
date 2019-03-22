3d2e412ac514b0a6c2235f71ba7f6ef7f365c6a2
George K <gkh36@uclive.ac.nz>
2019-03-22 13:09:16 +1300

    Added js file holding constants such as errors

---
+++ config/constants.js
@@ -0,0 +1,6 @@
+exports.WORKLOAD = 5;
+exports.AUTHERROR = {name:"Unauthorized", message:"Unauthorized"};
+exports.NOTFOUNDERROR = {name:"Not Found", message:"Not Found"};
+exports.FORBIDDENERROR = {name:"Forbidden", message:"Forbidden"};
+exports.BADREQUESTERROR = {name:"Bad Request", message:"Bad Request"};
+exports.UNIMPLEMENTED = {"Error" : "Unimplemented"};
